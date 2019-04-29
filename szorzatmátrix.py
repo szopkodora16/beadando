a7fe120f40414f2b6e6b6d7da08ff6b512e0e25e
szopkodora16 <szopkodora16@gmail.com>
2019-04-29 19:14:22 +0200

    mátrix

--- szorzatmátrix.py
+++ szorzatmátrix.py
@@ -1,5 +1,10 @@
-import sys
+
 import numpy as np
-n=np.random.randint(1,5,(int(sys.argv[1]),int(sys.argv[2])))
-m=np.random.randint(1,5,(int(sys.argv[3]),int(sys.argv[4])))
-print(n,m)
+n=np.random.randint(1,5,(2,4))
+m=np.random.randint(1,5,(4,3))
+print(n)
+print(m)
+for i in range(0,n.shape[0]):
+    for j in range(0,n.shape[1]):
+        print(n[i,j])
+
