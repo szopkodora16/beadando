8936bfb84bbbd55ca8dd17695406a87808e5f220
szopkodora16 <szopkodora16@gmail.com>
2019-04-29 19:07:20 +0200

    prím-e az osztó

--- közösosztó.py
+++ közösosztó.py
@@ -2,4 +2,7 @@ n=int(input("Addj meg egy számot."))
 m=int(input("Addj meg egy másik számot."))
 for i in range(1,m+1):
     if m%i==0 and n%i==0:
+        for szam in range(2,i+1):
+            if i%szam==0:
+                print("nem prím")
         print(i)
---
+++ szorzatmátrix.py
@@ -0,0 +1,5 @@
+import sys
+import numpy as np
+n=np.random.randint(1,5,(int(sys.argv[1]),int(sys.argv[2])))
+m=np.random.randint(1,5,(int(sys.argv[3]),int(sys.argv[4])))
+print(n,m)
