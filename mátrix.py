e37e9863a7d76d741aea56fad89e49f39735df96
szopkodora16 <szopkodora16@gmail.com>
2019-05-01 17:21:12 +0200

    mátrixos

---
+++ mátrix.py
@@ -0,0 +1,11 @@
+import numpy as np
+def valami(m,n):
+    c=0
+    for i in range(0,len(m)):
+        c += n[i]*m[i]
+    return c
+m=np.random.randint(10,50,10)
+print(m)
+n=np.random.randint(10,50,10)
+print(n)
+print(valami(m,n))
