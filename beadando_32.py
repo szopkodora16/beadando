147583da352d6dcecf0ce63f315b085f5562a7be
szopkodora16 <szopkodora16@gmail.com>
2019-05-11 15:01:25 +0200

    beadandó 32_es

--- beadando_32.py
+++ beadando_32.py
@@ -1,11 +1,12 @@
+import sys
 try:
-    n=int(input("addj meg egy számot"))
+    n=int(sys.argv[1])
     for i in range(1,n+1):
         s=" "
         for j in range(1,i+1):
             s+=str(j)
         a=s[::-1]
         s+=a[1:]
-        print("{:^100}".format(s))
+        print("{:^40}".format(s))
 except ValueError:
-    print("Nem számot adtál meg")
+    print("Nem számot adtál meg!")
