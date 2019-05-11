65979099c187a35edf7a975b3e83ab3f31a8ab06
Szopkó Dóra <47785067+szopkodora16@users.noreply.github.com>
2019-05-11 15:03:04 +0200

    Delete páros,páratlan.py

--- páros,páratlan.py
+++
@@ -1,7 +0,0 @@
-a = int(input('Give a number: '))
-while a != 0:
-    if a%2 == 0:
-        print('{} is even.'.format(a))
-    else:
-        print('{} is odd.'.format(a))
-    a = int(input('Give a number: '))
